0e50d4fc68b3b50512ddeb1cb21fd40e5a033fa0
Joe <joelihai2021@gmail.com>
2021-03-24 08:53:35 +0800

    calculate the length of the rwviews

--- reviews_analytics.py
+++ reviews_analytics.py
@@ -1,10 +1,17 @@
 
 data = []
 count = 0
+length = 0
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1
-		if count % 1000 == 0:
+		if count % 100000 == 0:
 			print(len(data))
-print(len(data))
+	i = 0
+	while i < len(data):
+		length = length + len(data[i])
+		i += 1
+print('The average lengrh of the reviews is', length / len(data)) 
+
+print('File read completed. There are ', len(data), 'records in total. ')
